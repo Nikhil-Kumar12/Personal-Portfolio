296f59651930f488d7eafd18d2e7b8a04b003c26
Nikhil Kumar <70438111+Nikhil-Kumar12@users.noreply.github.com>
2022-03-26 15:08:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,12 +12,10 @@ app.use(cors());
 
 app.use("/", contactRoute);
 
- if (process.env.NODE_ENV === "production") {
-   app.use(express.static("client/build"));
-   app.get("*", (req, res) =>
-     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-   );
- }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "client/public", "build", "index.html")));
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, console.log(`server listing to port 5000 only`));
