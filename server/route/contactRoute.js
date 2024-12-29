08855eb0b20dcce4c3628e8c0613483c4261f1fb
Nikhil Kumar <nbaghel12@gmail.com>
2024-12-29 21:00:08 +0530

    Separated Backend

--- server/route/contactRoute.js
+++ server/route/contactRoute.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 const nodemailer = require("nodemailer");
-
 router.post("/contact", (req, res) => {
   let data = req.body;
-  if (data.name.length === 0 || data.email.length === 0 || data.message.length === 0) {
+  if (
+    data.name.length === 0 ||
+    data.email.length === 0 ||
+    data.message.length === 0
+  ) {
     return res.json({ msg: "Please fill all the fields" });
   }
 
@@ -30,7 +33,8 @@ router.post("/contact", (req, res) => {
 
   smtpTransporter.sendMail(mailOptions, (error) => {
     try {
-      if (error) return res.status(400).json({ msg: "Please fill all the fields" });
+      if (error)
+        return res.status(400).json({ msg: "Please fill all the fields" });
       res.status(200).json({ msg: "Thank you for contacting Nikhil!" });
     } catch (error) {
       if (error) return res.status(500).json({ msg: "There is server error" });
